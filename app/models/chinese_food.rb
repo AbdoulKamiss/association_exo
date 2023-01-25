88f8bcb5ea0368fbd108ed74b1cbf9243fbd0af2
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-01-25 17:07:01 +0000

    commit assoc

--- app/models/chinese_food.rb
+++ app/models/chinese_food.rb
@@ -1,2 +1,3 @@
 class ChineseFood < ActiveRecord::Base
+    belongs_to :type , class_name: "Food"
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+    belongs_to :customer
+    belongs_to :adress
+    has_many :order_foods
 end
